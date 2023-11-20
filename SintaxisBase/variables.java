dcb8cc57297d990e07edf1c4af595ba81e1b2a50
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-20 02:03:45 -0600

    Declaración de Variables en JAVA

---
+++ SintaxisBase/variables.java
@@ -0,0 +1,22 @@
+package com.mycompany.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        // !Declaración de variables
+        int Edad = 20; // * Entero
+        String Nombre = "Diego", Apellido = "López"; // * Cadenas
+        double Estatura = 1.85; // * Decimales
+
+        // ? System.out.println(); Para mostrar en pantalla
+        // ! Signo "más" (+) Para concatenar variables
+
+        System.out.println("Hello World!");
+        System.out.println("Datos Personales:");
+        System.out.println("Nombre: " + Nombre + " " + Apellido);
+        System.out.println("Edad: " + Edad);
+        System.out.println("Estatiura: " + Estatura);
+
+    }
+
+}
